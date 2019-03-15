522e6cb00fdaf369576de22cb5a1560eeef403cd
hatdeck <daniel.bryce.hall@gmail.com>
2019-03-14 18:03:25 -0700

    documentation

--- cha2019-03-13/camelize.py
+++ cha2019-03-13/camelize.py
@@ -1,4 +1,19 @@
+"""
+You must create a method that can convert a string from any format into PascalCase. This must support symbols too.
+
+Don't presume the separators too much or you could be surprised.
+
+For example: (Input --> Output)
+
+"example name" --> "ExampleName"
+"your-NaMe-here" --> "YourNameHere"
+"testing ABC" --> "TestingAbc"
+"""
+
+
 import string
+
+
 
 def camelize(str_):
     s = str_.translate(string.maketrans(string.punctuation, " " * len(string.punctuation)))
