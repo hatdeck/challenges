d020d9cd4cec30ed3ad845508b8a7ec0f197fdae
hatdeck <daniel.bryce.hall@gmail.com>
2020-02-27 20:35:25 -0700

    documentation

--- cha2020-02-26/is_prime.py
+++ cha2020-02-26/is_prime.py
@@ -1,3 +1,14 @@
+"""
+Consider an array that has no prime numbers, and none of its elements has any prime digit. It would start with: [1,4,6,8,9,10,14,16,18,40,44..].
+
+12 and 15 are not in the list because 2 and 5 are primes.
+
+You will be given an integer n and your task will be return the number at that index in the array. For example:
+
+solve(0) = 1
+solve(2) = 6
+"""
+
 def is_prime(n):
     if n < 3: return n == 2
     if not n%2: return False
