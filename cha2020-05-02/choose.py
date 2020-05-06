4d4e6d8de8c9401061fd238abc696f0c1d771199
hatdeck <daniel.bryce.hall@gmail.com>
2020-05-05 20:43:24 -0700

    added reg_sum_hits

--- cha2020-05-02/choose.py
+++ cha2020-05-02/choose.py
@@ -12,4 +12,7 @@ def choose(n, k):
 
 def numrolls(k, n, s):
     return sum((-1)**i * choose(n, i) * choose(k-s*i-1, k-s*i-n) for i in range((k-n)//s + 1))
-    
+
+
+def reg_sum_hits(n, s):
+    return [[k, numrolls(k, n, s)] for k in range(n, s*n+1)]
