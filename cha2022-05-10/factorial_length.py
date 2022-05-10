8e5375cc7005ee2c709bcf4d06d29c2eed854ff6
hatdeck <daniel.bryce.hall@protonmail.com>
2022-05-10 00:22:59 -0700

    May 10 challenge

--- cha2022-05-10/factorial_length.py
+++ cha2022-05-10/factorial_length.py
@@ -1,5 +1,5 @@
 """
-In this Kata, you will implement a function count that takes an integer and returns the number of digits in factorial(n).
+Implement a function count that takes an integer and returns the number of digits in factorial(n).
 
 For example, count(5) = 3, because 5! = 120, and 120 has 3 digits.
 
